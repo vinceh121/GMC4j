7889a4d5d9f9bffb64acf567e451d379e48ebfc7
vinceh121 <vincent.hyvert@sfr.fr>
2019-06-18 14:35:40 +0200

    Added gyroscope and date functions. From com port

--- src/main/java/me/vinceh121/gmc/GMC.java
+++ src/main/java/me/vinceh121/gmc/GMC.java
@@ -7,9 +7,11 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.math.BigDecimal;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.charset.Charset;
+import java.util.Calendar;
 
 import com.fazecast.jSerialComm.SerialPort;
 
@@ -19,12 +21,15 @@ public class GMC {
 	private Charset ascii;
 	private SerialPort port;
 
-	public static GMC fromComPort(String comPort, int baudRate) {
-		SerialPort port = SerialPort.getCommPort(comPort);
+	public static GMC fromComPort(SerialPort port, int baudRate) {
 		port.setBaudRate(baudRate);
 		port.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 1000, 1000);
 		port.openPort();
 		return new GMC(port.getInputStream(), port.getOutputStream());
+	}
+	
+	public static GMC fromComPort(String comPort, int baudRate) {
+		return fromComPort(SerialPort.getCommPort(comPort), baudRate);
 	}
 
 	public GMC(File stream) throws FileNotFoundException {
@@ -37,17 +42,67 @@ public class GMC {
 
 		this.ascii = Charset.forName("US-ASCII");
 	}
-	
-	
-	
+
+	public short getGyroX() throws IOException {
+		byte[] arr = sendCommand(GMCCommand.GETGYRO);
+		return (short) (((arr[0] & 0xFF) << 8) | (arr[1] & 0xFF));
+	}
+
+	public short getGyroY() throws IOException {
+		byte[] arr = sendCommand(GMCCommand.GETGYRO);
+		return (short) (((arr[2] & 0xFF) << 8) | (arr[3] & 0xFF));
+
+	}
+
+	public short getGyroZ() throws IOException {
+		byte[] arr = sendCommand(GMCCommand.GETGYRO);
+		return (short) (((arr[4] & 0xFF) << 8) | (arr[5] & 0xFF));
+	}
+
+	public float getTemperature() throws IOException {
+		byte[] arr = sendCommand(GMCCommand.GETTEMP);
+
+		if (arr[3] != 0xAA)
+			throw new IOException("4th byte isn't 0xAA: " + Integer.toHexString(Byte.toUnsignedInt(arr[3])));
+
+		BigDecimal dec = new BigDecimal(arr[0]);
+		dec = dec.scaleByPowerOfTen(dec.precision());
+		dec = dec.add(new BigDecimal(arr[1]));
+		dec = dec.movePointLeft(new BigDecimal(arr[1]).precision());
+		if (arr[2] == 0)
+			dec = dec.plus();
+		else
+			dec = dec.negate();
+		return dec.floatValue();
+	}
+
+	public Calendar getDateTime() throws IOException {
+		byte[] arr = sendCommand(GMCCommand.GETDATETIME);
+		if (arr[6] != 0xAA) { // Last byte is constant
+			throw new IOException("Last byte is not equal to 0xAA: " + arr[6]);
+		}
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.YEAR, arr[0]);
+		cal.set(Calendar.MINUTE, arr[1]);
+		cal.set(Calendar.DAY_OF_MONTH, arr[2]);
+		cal.set(Calendar.HOUR, arr[3]);
+		cal.set(Calendar.MINUTE, arr[4]);
+		cal.set(Calendar.SECOND, arr[5]);
+		return cal;
+	}
+
+	public String getConfig() throws IOException {
+		return new String(ascii.decode(ByteBuffer.wrap(sendCommand(GMCCommand.GETCFG))).array());
+	}
+
 	public void reboot() throws IOException {
 		sendCommand(GMCCommand.REBOOT);
 	}
-	
+
 	public void powerOn() throws IOException {
 		sendCommand(GMCCommand.POWERON);
 	}
-	
+
 	public void powerOff() throws IOException {
 		sendCommand(GMCCommand.POWEROFF);
 	}
@@ -84,7 +139,7 @@ public class GMC {
 			return buf;
 		}
 	}
-	
+
 	public void close() throws IOException {
 		input.close();
 		output.close();
