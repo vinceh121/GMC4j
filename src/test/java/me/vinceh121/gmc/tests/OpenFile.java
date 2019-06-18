fe309776d26073e939ae0b4064be25d32c9805c1
vinceh121 <vincent.hyvert@sfr.fr>
2019-06-18 14:36:05 +0200

    Added more tests

--- src/test/java/me/vinceh121/gmc/tests/OpenFile.java
+++ src/test/java/me/vinceh121/gmc/tests/OpenFile.java
@@ -3,16 +3,25 @@ package me.vinceh121.gmc.tests;
 import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
+import java.util.Arrays;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import me.vinceh121.gmc.GMC;
+import me.vinceh121.gmc.GMCCommand;
 
 public class OpenFile {
+	private GMC gmc;
+
+	@Before
+	public void setup() {
+		gmc = GMC.fromComPort("/dev/ttyUSB0", 115200);
+	}
 
 	@Test
 	public void test() throws IOException {
-		GMC gmc = GMC.fromComPort("/dev/ttyUSB0", 115200);
+
 		assertNotNull(gmc.getVersion());
 		System.out.println("Version: " + gmc.getVersion());
 
@@ -21,6 +30,25 @@ public class OpenFile {
 
 		assertNotNull(gmc.getVoltage());
 		System.out.println("Voltage: " + gmc.getVoltage());
+
+	}
+
+	@Test
+	public void dateTime() throws IOException {
+		assertNotNull(gmc.getDateTime());
+		System.out.println(gmc.getDateTime().toString());
+	}
+
+	@Test
+	public void config() throws IOException {
+		assertNotNull(gmc.sendCommand(GMCCommand.GETCFG));
+		System.out.println("Cfg: " + Arrays.toString(gmc.sendCommand(GMCCommand.GETCFG)));
+	}
+	
+	@Test
+	public void temperature() throws IOException {
+		assertNotNull(gmc.getTemperature());
+		System.out.println(gmc.getTemperature());
 	}
 
 }
